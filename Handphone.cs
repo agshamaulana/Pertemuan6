f37edf456cc9da16dde383e71b1d492dc7aff131
agshamaulana <agshamaulana14@students.amikom.ac.id>
2023-07-12 13:54:11 +0700

    first commit

---
+++ Handphone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsuletion
+{
+    internal class Handphone
+    {
+        //field
+        public string hp;
+        public string merk;
+        protected string emei;
+
+        //propeties
+        public string Hp
+        {
+            get { return hp; }
+            set { hp = value; }
+        }
+        public string Merk
+        {
+            get { return merk; }
+            set { merk = value; }
+        }
+
+        public string Emei
+        {
+            get { return emei; }
+            set { emei = value; }
+        }
+        //method
+        public void SetHandphone(string hp, string merk)
+        {
+            this.hp = hp;
+            this.merk = merk;
+            Console.WriteLine("Type Handphone : {0}", this.hp);
+            Console.WriteLine("Merk Handphone : {0}", this.merk);
+            Console.ReadKey();
+        }
+        public void SetEmei(string emei)
+        {
+            this.emei = emei;
+            Console.WriteLine("Nomor Emei : {0}", this.emei);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsuletion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Handphone hp1 = new Handphone();
+            hp1.Hp = "Android";
+            hp1.Merk = "Odoo";
+            hp1.Emei = "1244-4623-7654";
+
+            Console.WriteLine($"Type Handphone : {hp1.Hp}" + "\n" + $"Merk Handphone : {hp1.Merk}" + "\n" + $"Nomor Emei : {hp1.Emei}");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
+
+            Handphone hp2 = new Handphone();
+            hp2.SetHandphone("Iphone", "Iphone 14 Promag");
+            hp2.SetEmei("4823-7765-3246");
+            Console.WriteLine(hp2);
+
+            Console.ReadKey();
+        }
+    }
+}
